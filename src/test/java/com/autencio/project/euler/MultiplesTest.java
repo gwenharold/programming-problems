127f53f49e0fdd4e4af4a354214a66fb5f40ccf7
Gwen Autencio <gwen.autencio@gmail.com>
2016-03-30 23:00:52 +1100

    fix test setup

---
+++ src/test/java/com/autencio/project/euler/MultiplesTest.java
@@ -0,0 +1,14 @@
+package com.autencio.project.euler;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+// TODO : setup unit testing in project
+public class MultiplesTest {
+
+	@Test
+	public void testFirstTest() {
+		Assert.assertTrue(true);
+	}
+
+}
